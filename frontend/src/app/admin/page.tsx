d38c4777005bf879c0904705d88d9ef69cf16a0a
jemiomiguelangel <tu@email.com>
2026-03-21 17:59:33 -0400

    fix: cerrar correctamente panel dashboard admin

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -204,6 +204,7 @@ export default function AdminDashboard() {
               )}
             </div>
             </div>
+          )}
 
           {/* REGISTRO */}
           {activePanel === 'registro' && (
