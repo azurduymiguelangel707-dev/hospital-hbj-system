ae0d383afa3a3f0b1f93db546adab8bcb4f1e4f2
k46017143-alt <k46017143@gmail.com>
2026-09-01 00:32:20 -0400

    fix: correct user entity import path

--- backend/src/seed.controller.ts
+++ backend/src/seed.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from '../users/entities/user.entity';
+import { User } from './modules/users/entities/user.entity';
 import * as bcrypt from 'bcrypt';
 
 @Controller('seed')
